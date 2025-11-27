01d71d62c4601579c3ae483dacc8a1d39efd129a
SL-x-TnT <SL-x-TnT@users.noreply.github.com>
2025-11-27 10:06:28 -0500

    Update Consts.cs

--- OreBetWatcher/Ore/Consts.cs
+++ OreBetWatcher/Ore/Consts.cs
@@ -13,9 +13,5 @@ namespace OreBetWatcher.Ore
 
         public static readonly string OreTokenMint = "oreoU2P8bN6jkk3jbaiVxYnG1dCXcYxwhwyK9jSybcp";
         public static readonly string SolTokenMint = "So11111111111111111111111111111111111111112";
-
-        public const string Rpc = "http://tx.arby.cc/a70b4c704b398a35de538ccfafac";
-        public const string JitoRpc = "https://mainnet.block-engine.jito.wtf";
-        public const string StreamRpc = "ws://tx.arby.cc/a70b4c704b398a35de538ccfafac";
     }
 }
